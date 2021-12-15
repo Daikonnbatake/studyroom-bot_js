5e7028c1f60e8efd7a8235b8a8851d40af0d55c3
Daikonnbatake <taisei.nmz.dsk@gmail.com>
2021-12-15 07:56:40 +0000

    DBアクセスラッパーがいい感じになった

--- scripts/utility/dbAccsess.js
+++ scripts/utility/dbAccsess.js
@@ -1,180 +1,49 @@
 const mysql = require('mysql2/promise');
 const FS = require('fs');
-const { chdir } = require('process');
 const Log = require('./log');
 
 class DBAccsess
 {
+    // 設定
+    static config = JSON.parse(FS.readFileSync(`${process.cwd()}/meta/db-conf.json`, 'utf-8'));
+
     // DBに接続
-    static async _getConnection()
+    static async _getConnect()
     {
-        const cwd = process.cwd();
-        const conf = JSON.parse(FS.readFileSync(cwd + '/meta/db-conf.json', 'utf-8'));
-
-        try
-        {
-            return await mysql.createConnection(conf);
-        }
-        catch(e)
-        {
-            let log = new Log(Log.typeDict.error, '', '', e).print();
-        }
-        // 正常終了は true を返す
-        return true;
-    }
-
-    // id(主キー)を取得
-    static async getId(table, column, value)
-    {
-        const connect = await this._getConnection();
         let ret = null;
 
-        try
-        {
-            let query = 'SELECT * FROM ?? WHERE ?? = ?';
-            let data = [table, column, value];
-            ret = await connect.query(query, data);
-        }
-        catch(e)
-        {
-            let log = new Log(Log.typeDict.error, '', '', e).print();
-        }
-        finally
-        {
-            connect.end();
-        }
-        return ret[0];
+        try{ ret = await mysql.createConnection(this.config); }
+        catch(e){ let error = new Log(e).print()}
+
+        return ret;
     }
 
-    // カラムを取得
-    static async getColumn(table, column, where = null)
+    // ベーシックなクエリ送受信ラッパー
+    static async query(query, data=[])
     {
-        const connect = this._getConnection();
         let ret = null;
-        let queru;
+        let con = await this._getConnect();
+
         try
         {
-            if (where === null) query = 'SELECT ?? FROM ??';
-            else queru = `SELECT ?? FROM ?? WHERE ${where}`;
-            const data = [column, table];
-            ret = await connect.query(query, data);
+            ret = await con.query(query, data);
+            const successLog = new Log('DBに接続', `DBに接続しました。\nクエリ ${query} の実行を試みています...`).print();
         }
-        catch(e){ let log = new Log(Log.typeDict.error, '', '', e).print(); }
-        finally { connect.end(); }
-        return ret[0];
+
+        catch(e){ this._dbErrorLogging(e); }
+        finally{ con.end(); }
+
+        const resultLog = new Log('DBから切断', '通信は正常に終了しました。\nクエリの結果が返却されました。');
+        if (ret != null) resultLog.print();
+
+        return ret;
     }
 
-    // ユーザーを登録
-    static async addUser(user)
+    // DB側で発生したエラーのうち、UniqueKey重複エラー等重要度の低いエラー等は選別する。
+    static _dbErrorLogging(error)
     {
-        // 接続を作成
-        const connect = await this._getConnection();
-        try
-        {
-            // ユーザーデータ追加
-            let query = 'INSERT INTO users (discord_user_id) VALUES (?)';
-            let data = [user.id];
-            await connect.query(query, data);
-        }
-        // 諸々のエラーを無視
-        catch(e)
-        {
-            let log = new Log(Log.typeDict.error, '', '', e).print();
-        }
-        finally
-        {
-            // 接続解除
-            connect.end();
-        }
-
-        let log = new Log(Log.typeDict.message, 'ユーザー情報を登録', `ユーザー: ${user.username} (id: ${user.id})`);
-        // 正常終了は true を返す
-        return true;
-    }
-    // チャンネルを登録
-    static async addChannel(channel)
-    {
-        // 接続を作成
-        const connect = await this._getConnection();
-
-        try
-        {
-            // チャンネルデータ追加
-            let query = 'INSERT INTO channels (discord_channel_id) VALUES (?)';
-            let data = [channel.id];
-            await connect.query(query, data);
-        }
-        // 諸々のエラーを無視
-        catch(e)
-        {
-            let log = new Log(Log.typeDict.error, '', '', e).print();
-        }
-        finally
-        {
-            // 接続解除
-            connect.end();
-        }
-
-        let log = new Log(Log.typeDict.message, 'チャンネル情報を登録', `チャンネル: ${channel.name} (id: ${channel.id})`);
-        // 正常終了は true を返す
-        return true;
-    }
-    // サーバーを登録
-    static async addGuild(guild)
-    {
-        // 接続を作成
-        const connect = await this._getConnection();
-
-        try
-        {
-            // サーバーデータ追加
-            let query = 'INSERT INTO guilds (discord_guild_id) VALUES (?)';
-            let data = [guild.id];
-            await connect.query(query, data);
-        }
-        // 諸々のエラーを無視
-        catch(e)
-        {
-            let log = new Log(Log.typeDict.error, '', '', e).print();
-        }
-        finally
-        {
-            // 接続解除
-            connect.end();
-        }
-
-
-        let log = new Log(Log.typeDict.message, 'サーバー情報を登録', `サーバー: ${guild.name} (id: ${guild.id})`);
-        // 正常終了は true を返す
-        return true;
-    }
-
-    // vc入退室を記録
-    static async loggingVoice(guild, channel, user, isIn)
-    {
-        // 接続を作成
-        const connect = await this._getConnection();
-
-        try
-        {
-            // 各種idを取得
-            const guild_id = await this._getId('guilds', 'discord_guild_id', guild.id);
-            const channel_id = await this._getId('channels', 'discord_channel_id', channel.id);
-            const user_id = await this._getId('users', 'discord_user_id', user.id);
-
-            // voice_activities テーブルを更新
-            const query = 'INSERT INTO voice_activities (guild_id, channel_id, user_id, status) VALUES (?, ?, ?, ?)';
-
-            const data = [guild_id[0].id, channel_id[0].id, user_id[0].id, isIn];
-            await connect.query(query, data);
-        }
-
-        // 諸々のエラーを無視
-        catch(e){ let log = new Log(Log.typeDict.error, '', '', e).print(); }
-        finally { connect.end(); }
-
-        // 正常終了は true を返す
-        return true;
+        let errorLog = new Log(error);
+        errorLog.print();
     }
 }
 
