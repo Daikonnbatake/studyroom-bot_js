6fcb23c75057e4ff1c6bb94f287ab809596a54d2
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2021-12-24 21:54:37 +0900

    issues-7のバグを直した。 (#9)
    2敗

--- scripts/utility/confGen.js
+++ scripts/utility/confGen.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 
-function confGen()
+const ex = function confGen()
 {
     // DBの設定ファイルを生成
     const cwd = process.cwd();
     
-    if (fs.existsSync(`${cwd}/meta/db-conf.json`))
+    if (!fs.existsSync(`${cwd}/meta/db-conf.json`))
     {
-        const dbConfJson = { "host": "", "port": 3306, "user": "", "password": "", "database": "discord_srb"};
-        fs.writeFile(`${cwd}/meta/db-conf.json`, JSON.stringify(dbConfJson, null, '\t'));
+        const dbConfJson = {"host": "", "port": 3306, "user": "", "password": "", "database": "discord_srb"};
+        fs.writeFileSync(`${cwd}/meta/db-conf.json`, JSON.stringify(dbConfJson, null, 4));
     }
 
     // tokenの設定ファイルを生成
-    if (fs.existsSync(`${cwd}/meta/token.txt`))
+    if (!fs.existsSync(`${cwd}/meta/token.txt`))
     {
-        fs.writeFile(`${cwd}/meta/db-conf.json`, '[ここにbotのtokenを張る]');
+        fs.writeFileSync(`${cwd}/meta/token.txt`, '[ここにbotのtokenを張る]');
     }
 }
 
-module.exports = confGen;
+module.exports = ex;
