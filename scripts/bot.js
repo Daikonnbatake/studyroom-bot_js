3184b995ac11daf1f6601f9a1fce5dcee4a29906
Daikonnbatake <taisei.nmz.dsk@gmail.com>
2021-12-24 12:53:37 +0000

    issues-7のバグを直した。

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,18 +1,19 @@
-/* ボイスチャンネルの監視をやるクラス */
 
-
-/* ====================================================================================== */
+/* 存在しない場合confファイルを生成 */
+const confGen = require('./utility/confGen.js');
+confGen();
 
 /* インポート周り */
 const FS = require('fs');
 const DISCORD = require('discord.js');
+const Log = require('./utility/log');
 
-const confGen = require('./utility/confGen');
 const voiceObserver = require('./observer/voiceObserver.js');
 
 /* インテントのフラグたて */
 let intentFlag = new DISCORD.Intents
 ([
+	// サーバー
 	DISCORD.Intents.FLAGS.GUILDS,
 	DISCORD.Intents.FLAGS.GUILD_MEMBERS,
 	// DISCORD.Intents.FLAGS.GUILD_BANS,
@@ -25,6 +26,8 @@ let intentFlag = new DISCORD.Intents
 	DISCORD.Intents.FLAGS.GUILD_MESSAGES,
 	DISCORD.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 	// DISCORD.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+
+	// DM
 	DISCORD.Intents.FLAGS.DIRECT_MESSAGES,
 	DISCORD.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
 	// DISCORD.Intents.FLAGS.DIRECT_MESSAGE_TYPING
@@ -41,17 +44,19 @@ const TOKEN	 = FS.readFileSync('./meta/token.txt', 'utf-8');
 /* 各種定数 */
 const cwd = process.cwd();
 const commandPrefix = 'dev';
-const commandFiles = FS.readdirSync(cwd  + '/scripts/command').filter(file => file.endsWith('.js'));
+const commandFolders = FS.readdirSync(cwd + '/scripts/command');
 
 /* コマンドを読み込む */
-for(const file of commandFiles)
-{
-	const command = require(cwd + `/scripts/command/${file}`);
-	CLIENT.commands.set(command.name, command);
+for (const folder of commandFolders) {
+	const commandFiles = FS.readdirSync(`${cwd}/scripts/command/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`${cwd}/scripts/command/${folder}/${file}`);
+		CLIENT.commands.set(command.name, command);
+	}
 }
 
 /* コマンド解釈&実行 */
-CLIENT.on('messageCreate', message=>
+CLIENT.on('messageCreate', message =>
 {
 	// コマンド判定とBot無視
 	if (!message.content.startsWith(commandPrefix) || message.author.bot) return;
@@ -59,14 +64,17 @@ CLIENT.on('messageCreate', message=>
 	const args = message.content.slice(commandPrefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 	const command = CLIENT.commands.get(commandName);
-
+	
 	// 存在しないコマンドは無視
-	if(!CLIENT.commands.has(commandName)) return;
+	if(!CLIENT.commands.has(commandName)) return message.channel.send(`そんなコマンドねぇよボケがぁぁあ!`);
+	// 引数が足りない場合は受け付けない
+	if (command.args && !args.length) return message.channel.send(`ﾌﾟｷﾞｬｰｯ m9｡ﾟ(ﾟ^Д^ﾟ)ﾟ｡引数足りねぇwww 出直してこい ${message.author} www`);
+	// サーバーのみ実行可能なコマンドをDMから受け取った時は受け付けない
+	if (command.guildOnly && message.channel.type === 'dm') return message.reply('DMではつかえません、なんで負けたか明日までに考えといてください。');
+	// 権限を要するコマンドは管理者以外弾く
+	if (command.adminOnly && !message.guild.members.cache.find((member) => member.id === message.author.id).permissions.has('ADMINISTRATOR')) return message.reply('権限ね～よ～');
 
-	try
-	{
-		command.execute(message, args);
-	}
+	try { command.execute(message, args); }
 	catch(error)
 	{
 		console.error(error);
@@ -75,32 +83,27 @@ CLIENT.on('messageCreate', message=>
 });
 
 /* vc 監視 */
-CLIENT.on('voiceStateUpdate', (oldState, newState)=>
+
+CLIENT.on('voiceStateUpdate', async (oldState, newState)=>
 {
 	let user = oldState.member.user;
 	let newCh = newState.channel;
 	let oldCh = oldState.channel;
-	let newGuild = newState.guild;
-	let oldGuild = oldState.guild;
 
 	// 入室
-	if((oldState.channelId === null) && (newState.channelId != null))
-		voiceObserver.VoiceIn(newGuild, newCh, user);
-	
+	if((oldState.channelId === null) && (newState.channelId != null)) await voiceObserver.VoiceIn(newCh.guild, newCh, user);
+
 	// 退室
-	if((oldState.channelId != null) && (newState.channelId === null))
-		voiceObserver.VoiceOut(oldGuild, oldCh, user);
+	if((oldState.channelId != null) && (newState.channelId === null)) await voiceObserver.VoiceOut(oldCh.guild, oldCh, user);
 
 	// 移動
 	if ((oldState.channelId != null) && (newState.channelId != null) && (oldState.channelId != newState.channelId))
 	{
-		voiceObserver.VoiceIn(newGuild, newCh, user);
-		voiceObserver.VoiceOut(oldGuild, oldCh, user);
+		await voiceObserver.VoiceOut(oldCh.guild, oldCh, user);
+		await voiceObserver.VoiceIn(newCh.guild, newCh, user);
 	}
 });
 
-/* 設定ファイル生成 */
-confGen;
 
 /* 実行 */
 CLIENT.login(TOKEN);
