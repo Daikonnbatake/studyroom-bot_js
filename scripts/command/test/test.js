2b080e5fc56d38f09001678196f152dfb33b6691
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2021-12-26 23:05:43 +0900

    Issues 17 (#19)
    * #17 の原因特定のためのデバッグ用コマンドを実装
    
    * #17 の原因特定のためのデバッグ用コマンドを実装(その2)

--- scripts/command/test/test.js
+++ scripts/command/test/test.js
@@ -1,13 +1,22 @@
 const Embed = require('../../utility/customEmbed');
 const Time = require('../../utility/timeUtility');
 
+const TextDeco = require('../../utility/textDecorator');
+const getVoiceActivities = require('../../query/getVoiceActivities');
+
 async function body(message, args)
 {
     let e = new Embed;
+    const studyTime = await getVoiceActivities(message.author);
+    
+    let log = '';
+    for (const i of studyTime) { log += `${i.status} ${i.timestamp.toLocaleString('ja')}\n`; }
+    
     e.setTitle('テスト送信');
     e.addField('今日の00:00:00', Time.getToday().toLocaleString('ja'), true);
     e.addField('昨日の23:59:59', Time.getYesterday().toLocaleString('ja'), true);
     e.addField('一週間前の00:00:00', Time.getOneWeekAgo().toLocaleString('ja'), true);
+    e.addField('VC履歴', TextDeco.codeblock(log));
 
     message.channel.send({embeds: [e.embed]});
 }
