5bef837a791c4d9dae46e24ac3217b6945b7041d
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2021-12-26 22:49:55 +0900

    #17 の原因特定のためのデバッグ用コマンドを実装 (#18)

--- scripts/command/test/test.js
+++ scripts/command/test/test.js
@@ -1,8 +1,15 @@
 const Embed = require('../../utility/customEmbed');
+const Time = require('../../utility/timeUtility');
 
 async function body(message, args)
 {
-    message.channel.send('てすとめっせ～じ');
+    let e = new Embed;
+    e.setTitle('テスト送信');
+    e.addField('今日の00:00:00', Time.getToday().toLocaleString('ja'), true);
+    e.addField('昨日の23:59:59', Time.getYesterday().toLocaleString('ja'), true);
+    e.addField('一週間前の00:00:00', Time.getOneWeekAgo().toLocaleString('ja'), true);
+
+    message.channel.send({embeds: [e.embed]});
 }
 
 async function func(message, args)
