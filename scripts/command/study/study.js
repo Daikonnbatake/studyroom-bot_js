08dd74197149e21635e8f96a9f67e816d4bc6927
Daikonnbatake <taisei.nmz.dsk@gmail.com>
2021-12-30 01:36:39 +0000

    ランク(仮) 実装

--- scripts/command/study/study.js
+++ scripts/command/study/study.js
@@ -4,6 +4,7 @@ const getUserID = require('../../query/getUserID');
 const Log = require('../../utility/log');
 const TimeUtilitiy = require('../../utility/timeUtility');
 const ImageGen = require('../../utility/imageGen');
+const StudyRank = require('../../utility/studyRank');
 
 async function func(message, args)
 {
@@ -11,13 +12,19 @@ async function func(message, args)
     {
         const ret = await getVoiceActivities(message.author);
         const total = await getTotalStudyTime(message.author);
-
         const userID = await getUserID(message.author);
-
+        
+        
         const user = message.author;
-        const header = {userID: userID, userName: user.username, userIconURL: user.avatarURL(), userTitle: 'カフェイン中毒者'};
         const badge = ['_dummy', '_dummy', '_dummy'];
         const study = {total: total, study: TimeUtilitiy.getStudyTime(ret)};
+        
+        // ランク取得
+        let tmp = 0;
+        for (const i of study.study) tmp += i;
+        const rank = StudyRank.getUserRank(tmp);
+        
+        const header = {userID: userID, userName: user.username, userIconURL: user.avatarURL(), userTitle: rank};
 
         for (let i = study.study.length; i < 7; i++) study.study.push(0);
 
